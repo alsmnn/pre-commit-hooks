56b08688495e3c4a5300c78c356ec19a18698b86
Aljoscha Gerber <gerber@dkms-lab.de>
2023-08-17 12:32:29 +0200

    fix: remove unnecessary files

--- poetry-update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ -f poetry.lock ]]; then
-    DIFF=$(git diff --shortstat "$PRE_COMMIT_FROM_REF".."$PRE_COMMIT_TO_REF" poetry.lock)
-    if [[ $DIFF != "" ]]; then
-        poetry install
-    fi
-fi
